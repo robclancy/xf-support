f12b2e97b1c491d6e0c52d218d64b6eda6bdeba3
robclancy <robert@mercenarydesign.net>
2013-03-19 01:35:27 +1100

    commit to test composer

--- src/Robbo/Support/DataModelInterface.php
+++ src/Robbo/Support/DataModelInterface.php
@@ -4,5 +4,7 @@ interface DataModelInterface {
 
 	public function getResourceById($id, array $fetchOptions = array());
 
+	public function getAllResources(array $fetchOptions = array());
+
 	public function getResourceJoinOptions(array $fetchOptions);
 }
